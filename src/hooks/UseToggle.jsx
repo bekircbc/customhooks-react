5be32e3fe424b61314218c5c816d897f206ea586
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-31 09:42:40 +0200

    changed Use toggle hook..

---
+++ src/hooks/UseToggle.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export const useToggle = (_value) => {
+	const [value, setValue] = useState(_value);
+	const func = () => {
+		console.log('in the func');
+	};
+	return [value, func];
+};
---
+++ src/pages/PageUseToggle.jsx
@@ -0,0 +1,19 @@
+import { useToggle } from '../hooks/useToggle';
+
+export const PageUseToggle = () => {
+	const [userIsOnline, toggleUserIsOnline] = useToggle(false);
+
+	return (
+		<>
+			<h2>useToggle</h2>
+			{userIsOnline ? (
+				<p>User is currently online.</p>
+			) : (
+				<p>User is logged out.</p>
+			)}
+			<button onClick={() => toggleUserIsOnline()}>
+				Toggle online status
+			</button>
+		</>
+	);
+};
