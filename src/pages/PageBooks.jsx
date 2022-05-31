59aa5cf8e7e5d7ff17c80e11ca2a209e4845ce41
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-31 10:52:03 +0200

    book added to PageBooks with useFetch custom hook..

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,8 +1,43 @@
+import { useFetch } from '../hooks/UseFetch';
+
 export const PageBooks = () => {
+	const { items: techBooks, itemTotal: totalBooks } = useFetch(
+		'https://edwardtanguay.netlify.app/share/techBooks.json'
+	);
+	const { items: employees, isLoading: employeesAreLoading } = useFetch(
+		'https://edwardtanguay.netlify.app/share/employees.json',
+		true
+	);
+	const { itemTotal: totalFlashcards, isLoading: flashcardsAreLoading } =
+		useFetch(
+			'https://edwardtanguay.netlify.app/share/flashcards.json',
+			true
+		);
+
 	return (
 		<div className="page_books">
-			<h2>Books</h2>
-			<p>This is the book page.</p>
+			<p>
+				{employeesAreLoading ? (
+					<p>Employees are loading...</p>
+				) : (
+					<p>
+						Note: There are {employees.length} employees who have
+						access to this page.
+					</p>
+				)}
+			</p>
+			{flashcardsAreLoading ? (
+				<p>Flashcards are loading...</p>
+			) : (
+				<p>There are {totalFlashcards} flashcards about books.</p>
+			)}
+			<h2>There are {totalBooks} tech books.</h2>
+
+			<ul>
+				{techBooks.map((techBook, index) => {
+					return <li key={index}>{techBook.title}</li>;
+				})}
+			</ul>
 		</div>
 	);
 };
